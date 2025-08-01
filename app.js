7aa7cc90780bfbfc3b92bae11c882181eaa10832
riddhi <chatllat.mail@gmail.com>
2025-08-01 17:38:14 +0530

    solve cors error on deployment

--- app.js
+++ app.js
@@ -34,40 +34,23 @@ const PORT = process.env.PORT || 5000;
 await connectDB();
 
 const app = express();
-// app.use(cors({ origin: "*" }));
+app.use(cors());
+
 // const allowedOrigins = [
-//     "http://localhost:5173",
-//     "http://localhost:5174",
-//     "http://localhost:5000",
-//     "http://chatllatclient.ap-south-1.elasticbeanstalk.com",
-//     "http://chatllat-server.ap-south-1.elasticbeanstalk.com",
+//   "http://localhost:5173",
+//   "http://chatllatclient.ap-south-1.elasticbeanstalk.com"
 // ];
 
-// app.use(
-//     cors({
-//         origin: allowedOrigins,
-//         credentials: true,
-//     })
-// );
-
-const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://chatllatclient.ap-south-1.elasticbeanstalk.com",
-];
-
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-    })
-);
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
